19bb9e6a8508a508295017c754450078f00d0de9
YevheniiSkrypkin <81817746+YevheniiSkrypkin@users.noreply.github.com>
2021-06-29 15:35:52 +0200

    Add files via upload

--- ДЗ-13/js/script.js
+++ ДЗ-13/js/script.js
@@ -1,9 +1,13 @@
 function mult(x, y = 1) {
-    let result = Math.pow(x, y);     
-    return result;
+    if(isNaN(x) || isNaN(y)) {
+        return alert('error')
+    }
+    else {
+        return alert(Math.pow(x, y))
+    }
 }
-  
+
 let x = +prompt('First num');
 let y = +prompt('Second num');  
 
-let res = isNaN(x) || isNaN(y) ? alert('Error, it is NaN') : alert( mult(x, y) );
+let result = mult(x, y);
